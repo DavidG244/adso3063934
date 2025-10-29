fbe5ca9fb99bb3edadb32bd8ca2a5b4e7a3a9f71
DavidG <david72ty@gmail.com>
2025-10-29 13:27:32 -0500

    oli

--- database/factories/PetFactory.php
+++ database/factories/PetFactory.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Pet>
  */
+
 class PetFactory extends Factory
 {
     /**
